fc89ee11d5c1c6fb94dd394817afd944fc3f23fa
amazing-username <kundeng94@gmail.com>
2019-02-14 21:50:50 -0500

    Done some work for #27. Not final.

--- FreshAir/FreshAir/Management/SettingManager.cs
+++ FreshAir/FreshAir/Management/SettingManager.cs
@@ -30,14 +30,15 @@ namespace FreshAir.Management
         {
             if (!UserSettings.DarkTheme)
             {
-                App.Current.Resources["NavigationPrimary"] = Color.FromHex("#2196F3");
-                App.Current.Resources["ContentBackground"] = Color.FromHex("ffffff");
-                App.Current.Resources["Button"] = Color.FromHex("#787878");
+                App.Current.Resources["NavigationPrimary"] = Color.FromHex("#159f64");
+                App.Current.Resources["ContentBackground"] = Color.FromHex("f4f4f4");
+                App.Current.Resources["Button"] = Color.FromHex("#098751");
                 App.Current.Resources["ButtonText"] = Color.FromHex("#ffffff");
-                App.Current.Resources["TextColor"] = Color.FromHex("#999999");
+                App.Current.Resources["TextColor"] = Color.FromHex("#000000");
                 App.Current.Resources["Rando"] = Color.FromHex("#ffffff");
-                App.Current.Resources["LabelFrontground"] = Color.FromHex("#999999");
+                App.Current.Resources["LabelFrontground"] = Color.FromHex("#ffffff");
                 App.Current.Resources["PickerBackgroundColor"] = Color.FromHex("#787878");
+                App.Current.Resources["SwitchOn"] = Color.FromHex("#12a90d");
                 App.Current.Resources["PickerTextColor"] = Color.FromHex("#999999");
                 App.Current.Resources["SliderLineColor"] = Color.FromHex("#000000");
                 App.Current.Resources["SliderThumbColor"] = Color.FromHex("#f3f3f3");
@@ -46,12 +47,13 @@ namespace FreshAir.Management
             {
                 App.Current.Resources["NavigationPrimary"] = Color.FromHex("#2d2d2d");
                 App.Current.Resources["ContentBackground"] = Color.FromHex("#202020");
-                App.Current.Resources["Button"] = Color.FromHex("#787878");
-                App.Current.Resources["ButtonText"] = Color.FromHex("#ffffff");
-                App.Current.Resources["TextColor"] = Color.FromHex("#999999");
-                App.Current.Resources["Rando"] = Color.FromHex("#ffffff");
-                App.Current.Resources["LabelFrontground"] = Color.FromHex("#999999");
+                App.Current.Resources["Button"] = Color.FromHex("#09633c");
+                App.Current.Resources["ButtonText"] = Color.FromHex("#e5e5e5");
+                App.Current.Resources["TextColor"] = Color.FromHex("#e5e5e5");
+                App.Current.Resources["Rando"] = Color.FromHex("#e5e5e5");
+                App.Current.Resources["LabelFrontground"] = Color.FromHex("#ffffff");
                 App.Current.Resources["PickerBackgroundColor"] = Color.FromHex("#787878");
+                App.Current.Resources["SwitchOn"] = Color.FromHex("#12a90d");
                 App.Current.Resources["PickerTextColor"] = Color.FromHex("#999999");
                 App.Current.Resources["SliderLineColor"] = Color.FromHex("#ffffff");
                 App.Current.Resources["SliderThumbColor"] = Color.FromHex("#8686ff");
@@ -59,14 +61,15 @@ namespace FreshAir.Management
         }
         public void LoadDefault()
         {
-                App.Current.Resources["NavigationPrimary"] = Color.FromHex("#2196F3");
-                App.Current.Resources["ContentBackground"] = Color.FromHex("ffffff");
-                App.Current.Resources["Button"] = Color.FromHex("#787878");
+                App.Current.Resources["NavigationPrimary"] = Color.FromHex("#159f64");
+                App.Current.Resources["ContentBackground"] = Color.FromHex("f4f4f4");
+                App.Current.Resources["Button"] = Color.FromHex("#098751");
                 App.Current.Resources["ButtonText"] = Color.FromHex("#ffffff");
-                App.Current.Resources["TextColor"] = Color.FromHex("#999999");
+                App.Current.Resources["TextColor"] = Color.FromHex("#000000");
                 App.Current.Resources["Rando"] = Color.FromHex("#ffffff");
-                App.Current.Resources["LabelFrontground"] = Color.FromHex("#999999");
+                App.Current.Resources["LabelFrontground"] = Color.FromHex("#ffffff");
                 App.Current.Resources["PickerBackgroundColor"] = Color.FromHex("#787878");
+                App.Current.Resources["SwitchOn"] = Color.FromHex("#12a90d");
                 App.Current.Resources["PickerTextColor"] = Color.FromHex("#999999");
                 App.Current.Resources["SliderLineColor"] = Color.FromHex("#000000");
                 App.Current.Resources["SliderThumbColor"] = Color.FromHex("#f3f3f3");
