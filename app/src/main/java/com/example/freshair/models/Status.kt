ad3a8e6c629235e312cdbf51a3befc958a088f78
amazing-username <kundeng94@gmail.com>
2019-02-20 22:47:56 -0500

    Back to the drawing board

--- app/src/main/java/com/example/freshair/models/Status.kt
+++
@@ -1,6 +0,0 @@
-package com.example.freshair.models
-
-import java.util.Date
-
-class Status(val message: String, val state: String, val date: Date, val user: String) {
-}
--- app/src/main/java/com/example/freshair/models/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.freshair.models
-
-class User(val firstname: String, val lastname: String, val email: String,
-           val username: String, val password: String) {
-}
